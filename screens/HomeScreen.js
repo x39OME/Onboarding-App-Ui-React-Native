e288edbe32958080ccc807f5df5f9184d0e85207
Sam <74021826+x39OME@users.noreply.github.com>
2024-02-20 20:55:29 +0300

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,54 @@
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Lottie from 'lottie-react-native';
+import LottieView from 'lottie-react-native';
+import { useNavigation } from '@react-navigation/native';
+import { removeItem } from '../utils/asyncStorage';
+
+const {width, height} = Dimensions.get('window');
+
+export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  const handleReset = async ()=>{
+    await removeItem('onboarded');
+    navigation.push('Onboarding');
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.lottie}>
+        <Lottie style={{flex:1}} source={require('../assets/animations/animation04.json')} autoPlay loop />
+      </View>
+      <Text style={styles.text}>Home Page</Text>
+      {/* Reset Button */}
+      <TouchableOpacity onPress={handleReset} style={styles.resetButton}>
+        <Text style={{color:'white', fontSize:16}}>Reset</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor:'white'
+  },
+  lottie:{
+    width: width*0.9,
+    height: width,
+  },
+  text: {
+    fontSize: width*0.09,
+    marginBottom: 20,
+  },
+  resetButton: {
+    backgroundColor:'#00A4B7',
+    padding:10,
+    borderRadius: 10,
+    width:100,
+    display:'flex',
+    alignItems:'center'
+  }
+})
---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,89 @@
+import {View, Text, StyleSheet, Dimensions, TouchableOpacity} from 'react-native'
+import React from 'react'
+import Onboarding from 'react-native-onboarding-swiper';
+import Lottie from 'lottie-react-native';
+import LottieView from 'lottie-react-native';
+import {useNavigation} from '@react-navigation/native';
+import {setItem} from '../utils/asyncStorage';
+
+const {width, height} = Dimensions.get('window');
+
+export default function OnboardingScreen() {
+  const navigation = useNavigation();
+
+  const handleDone = ()=>{
+    navigation.navigate('Home');
+    setItem('onboarded', '1');
+  }
+
+  // Done Button 
+  const doneButton = ({...props})=>{
+    return (
+      <TouchableOpacity style={styles.doneButton} {...props}>
+        <Text>Done</Text>
+      </TouchableOpacity>
+      )
+      
+  }
+  return (
+    <View style={styles.container}>
+      <Onboarding
+        onDone={handleDone}
+        onSkip={handleDone}
+        bottomBarHighlight={true}
+        DoneButtonComponent={doneButton}
+        containerStyles={{paddingHorizontal: 15}}
+        pages={[
+          // Page 1
+          {
+            backgroundColor: '#bffbff',
+            image: (
+              <View style={styles.lottie}>
+                <LottieView style={{flex:1}} source={require('../assets/animations/animation01.json')} autoPlay loop />
+              </View>
+            ),
+            title: 'Study Hard',
+            subtitle: 'Subscribe this channel to take courses and learn more',
+          },
+          // Page 2
+          {
+            backgroundColor: '#F7CA79',
+            image: (
+              <View style={styles.lottie}>
+                <LottieView style={{flex:1}} source={require('../assets/animations/animation02.json')} autoPlay loop />
+              </View>
+            ),
+            title: 'Work Hard',
+            subtitle: 'Get your work done seamlessly without interruption',
+          },
+          // Page 3
+          {
+            backgroundColor: '#f3c5ff',
+            image: (
+              <View style={styles.lottie}>
+                <LottieView style={{flex:1}} source={require('../assets/animations/animation03.json')} autoPlay loop />
+              </View>
+            ),
+            title: 'Achieve your goals',
+            subtitle: 'By learning more, we help you aim higher',
+          },
+        ]}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  lottie:{
+    width: width*0.9,
+    height: width,
+    color: 'white'
+  },
+  doneButton: {
+    padding: 20,
+  }
+})
